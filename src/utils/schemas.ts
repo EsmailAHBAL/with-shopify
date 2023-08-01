69da86111cec5f35034ba73b88b147804a775b04
Leader <leader@Leader.Leader>
2023-08-02 00:17:35 +0100

    hello

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { COLLECTIONS } from "./graphql";
 
 export const configSchema = z.object({
   shopifyShop: z.string(),
@@ -62,11 +63,15 @@ export const VariantResult = z.object({
   price: MoneyV2Result,
 });
 
+export const  Collections = z.object({
+  id:z.string(),title:z.string()
+})
 export const ProductResult = z
   .object({
     id: z.string(),
     title: z.string(),
     handle: z.string(),
+    description: z.string(),
     images: z.object({
       nodes: z.array(ImageResult),
     }),
@@ -74,5 +79,7 @@ export const ProductResult = z
       nodes: z.array(VariantResult),
     }),
     featuredImage: ImageResult.nullable(),
+    collections : z.array(Collections)
+   
   })
   .nullable();
